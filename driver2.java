7568760d78cca9251e99704db9ac4b7beb22cfa4
sidsomi <sidbond80@gmail.com>
2025-09-17 12:00:46 -0700

    Create driver2.java

---
+++ driver2.java
@@ -0,0 +1,13 @@
+public class driver2 {
+    public static void main(String [] args)
+    {
+        Rectangle rect1 = new Rectangle(0, 10, 50, 12);
+        rect1.calcArea();
+        rect1.calcPerimeter();
+
+        Rectangle rect2 = new Rectangle(5, 115, 24, 12);
+        rect2.calcArea();
+        rect2.calcPerimeter();
+    }
+    
+}
